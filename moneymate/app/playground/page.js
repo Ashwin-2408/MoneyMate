2f7d5c4a57212ea84b916a13f8bfb0165326b869
AShwin <ashwin.thanga@gmail.com>
2025-05-20 15:24:50 +0530

    initial setup completed

---
+++ moneymate/app/playground/page.js
@@ -0,0 +1,13 @@
+export default function Page(){
+    return(
+        <main className="space-y-8">
+            <h1 className="text-4xl  mt-8">PlayGround</h1>
+            <div>
+                <h2 className="mb-4 text-lg font-mono">ComponentName</h2>
+                <hr className="mb-4 border-gray-200 dark:borader-gray-800"></hr>
+                <div> here goes the component name</div>
+
+            </div>
+        </main>
+    )
+}
