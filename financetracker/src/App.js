0714c6e6299a9eb90990824306ab5d17990a393f
Ashwin <ashwin.thanga@gmail.com>
2025-05-25 10:42:09 +0530

    setup complete

---
+++ financetracker/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <div className="bg-primary text-white p-4">
+      <h1 className="text-xl font-bold">Tailwind should work now!</h1>
+    </div>
+  );
+}
+
+export default App;
