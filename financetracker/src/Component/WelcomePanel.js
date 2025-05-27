7364ae5d82484c7dab55b00d0237a4ff22e57795
Ashwin <ashwin.thanga@gmail.com>
2025-05-27 19:43:26 +0530

    made responsive changes'

--- financetracker/src/Component/WelcomePanel.js
+++ financetracker/src/Component/WelcomePanel.js
@@ -63,8 +63,8 @@ const WelcomePanel = () => {
         so you don’t have to think about it.
       </motion.p>
 
-      <div className="w-full max-w-4xl h-[300px] sm:h-[380px] bg-white rounded-2xl shadow-xl p-4 sm:p-6">
-        <h2 className="text-lg sm:text-xl font-semibold mb-4 text-black">Income vs Expense Overview</h2>
+      <div className="w-full max-w-4xl h-[380px] md:h-[300px] bg-white rounded-2xl shadow-xl  p-2 md:p-6">
+        <h2 className="text-md md:text-xl pt-1 font-semibold mb-4 text-black">Income vs Expense Overview</h2>
 
         {isMobile ? (
           <ResponsiveContainer width="90%" height="90%">
