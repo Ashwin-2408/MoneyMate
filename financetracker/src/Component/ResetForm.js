2a5b14baa94234a8e778634594feb52fa491d2a7
Ashwin <ashwin.thanga@gmail.com>
2025-06-04 09:57:19 +0530

    Added reset balance functionality implemented firebase delete operation to remove all transactions

--- financetracker/src/Component/ResetForm.js
+++ financetracker/src/Component/ResetForm.js
@@ -1,7 +1,39 @@
 import React, { useState } from "react";
 import Input from "./Input";
+import { toast } from "react-toastify";
+import { getDocs, collection, deleteDoc, } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../firebase";
 
 function ResetForm() {
+  const [user] = useAuthState(auth);
+  const handle_reset = async () => {
+    if (!user?.uid) {
+      toast.error("User not logged in");
+      return;
+    }
+    if (confirm !== "Reset my Balance") {
+      toast.error("Words do not match!");
+    } else {
+      try {
+        const transactionRef = collection(
+          db,
+          "Users",
+          user.uid,
+          "Transactions"
+        );
+        const snapshot = await getDocs(transactionRef);
+        for (const document of snapshot.docs) {
+          console.log(document);
+          await deleteDoc((document.ref));
+        }
+
+        toast.success("Reset");
+      } catch (error) {
+        toast.error(error.message);
+      }
+    }
+  };
   const [confirm, setconfirm] = useState("");
   return (
     <div className="flex flex-col p-4 shadow-md">
@@ -26,7 +58,10 @@ function ResetForm() {
           placeholder="Enter your confirmation"
         ></Input>
         <div className="flex justify-end items-center">
-          <button className="flex items-center justify-center h-[40px] w-[100px] p-3 rounded-md shadow-sm bg-[#ae292f] text-white border border-solid">
+          <button
+            onClick={handle_reset}
+            className="flex items-center justify-center h-[40px] w-[100px] p-3 rounded-md shadow-sm bg-[#ae292f] text-white border border-solid"
+          >
             Reset
           </button>
         </div>
